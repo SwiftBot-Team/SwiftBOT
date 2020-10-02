be96e5a03dd259def3c7d321c97884d3c8bb23fc
SrWhale <52763403+SrWhale@users.noreply.github.com>
2020-10-02 13:20:17 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 require('dotenv/config')
+
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
 const SwiftInstance = require('./src/Swift');
 const GeralInstance = require('./src/Instance');
 
@@ -6,7 +19,7 @@ const instance = new GeralInstance()
 
 const client = new SwiftInstance(instance, { config: './config.json', fetchAllMembers: true })
 client.login().then(() => {
-  client.log('Logado com Sucesso!', { color: 'green', tags: ['Discord Client'] });
+  client.log('Logado com Sucesso!', { color: 'green', tags: ['Discord Client'] })
 
   client.loadCommands(client.config.paths.commands)
   client.loadEvents(client.config.paths.events)
