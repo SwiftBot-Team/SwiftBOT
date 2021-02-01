70d5013cf9f88aaf2628f2190c0ed35da7a4d1e9
SrWhale <52763403+SrWhale@users.noreply.github.com>
2021-02-01 19:13:30 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
+require('dotenv/config')
+
+const SwiftInstance = require('./src/Swift');
+const GeralInstance = require('./src/Instance');
+
+const instance = new GeralInstance()
+
+const client = new SwiftInstance(instance, {
+  config: './config.json',
+  fetchAllMembers: true,
+  ws:
+  {
+    intents: ['GUILDS', 'GUILD_PRESENCES', 'GUILD_MESSAGES', 'GUILD_MEMBERS', 'GUILD_BANS', 'GUILD_EMOJIS', 'GUILD_MESSAGE_REACTIONS', 'DIRECT_MESSAGES']
+  }
+});
+
+client.login().then(() => {
+  client.log('Logado com Sucesso!', { color: 'green', tags: ['Discord Client'] })
+
+  client.connectLavalink();
+
+  client.loadCommands(client.config.paths.commands)
+  client.loadEvents(client.config.paths.events)
+  client.loadMongo();
+  client.loadFirebase();
+  client.initListeners();
+}).catch((err) => { console.log(err); client.log('Ocorreu um erro ao tentar logar! \n\n' + err, { color: 'red', tags: ['Discord Client'] }) })
+
+
 client.on('ready', () => {
 
   client.initLoaders();
@@ -17,3 +46,9 @@ client.on('ready', () => {
     }
   });
 })
+
+client.on('error', async err => console.log(err));
+
+client.on('err', async err => console.log(err));
+
+module.exports = client
