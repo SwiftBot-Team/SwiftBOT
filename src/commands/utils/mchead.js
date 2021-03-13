a7d81fb466318048effb21102c33d535e537610e
SrWhale <52763403+SrWhale@users.noreply.github.com>
2021-03-13 17:20:35 +0300

    Add files via upload

---
+++ src/commands/utils/mchead.js
@@ -0,0 +1,30 @@
+const Base = require("../../services/Command");
+const axios = require('axios')
+const Discord = require('discord.js')
+
+class McHead extends Base {
+  constructor(client) {
+    super(client, {
+      name: "mchead",
+      description: "descriptions:mchead",
+      usage: "usages:mchead",
+      category: "categories:utils",
+      cooldown: 1000,
+      aliases: ["mcc", "mccabeca"]
+    });
+  }
+
+  async run({ message, args, prefix }, t) {
+    let player = args.join(" ")
+
+    if(!player) {
+      return this.respond(t('commands:mchead.error'))
+    }
+
+    let url = `https://mc-heads.net/head/${player}.png`
+
+    message.channel.send({files: [url]});
+  }
+}
+
+module.exports = McHead;
